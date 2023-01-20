db0a0e5398238cee318dfd8b52ee7aa733399843
Luisa Nievas <luisa.nievas94@gmail.com>
2023-01-20 10:05:02 -0300

    Criando a função atualizarPlacar

--- app.js
+++ app.js
@@ -7,3 +7,45 @@ const PAPEL = "papel";
 const PEDRA = "pedra";
 const TESOURA = "tesoura";
 const jogadasPossiveis = [PAPEL, PEDRA, TESOURA];
+
+/*
+const imgPapel: Elemento img que mostra a imagem do papel 
+const imgPedra: Elemento img que mostra a imagem da pedra
+const imgTesoura: Elemento img que mostra a imagem da tesoura
+const divJogadaDoComputador: Elemento div que mostra a interrogaçãodo e vai mostrar a jogada do computador;
+const spanPlacarUsuario: Elemento span que mostra a pontuação do usuário;
+const spanPlacarComputador: Elemento span que mostra a pontuação do computador;
+const pPlacar: Elemento p que contem as duas spans que mostram os pontos;
+const audioDaVitoria: Elemento que contém o áudio da vitória;
+const audioDaDerrota: Elemento que contém audio da derrota;
+*/
+
+//CAPTURA DOS ELEMENTOS//
+const imgPapel = document.getElementById('imgPapel');
+const imgPedra = document.getElementById('imgPedra');
+const imgTesoura = document.getElementById('imgTesoura');
+
+const divJogadaDoComputador = document.getElementById('compChoice');
+
+const spanPlacarUsuario = document.getElementById('userScore');
+const spanPlacarComputador = document.getElementById('compScore');
+
+const sPlacar = document.getElementById('scores');
+
+const audioDaVitoria = document.getElementById('audioDaVitoria');
+const audioDaDerrota = document.getElementById('audioDaDerrota');
+
+/**
+ * 
+ * A função deve atualizar o placar na tela
+ * 
+ * @param {number} pontosDoUsuario 
+ * @param {number} pontosDoComputador 
+ */
+function atualizarPlacar(pontosDoUsuario, pontosDoComputador){
+    spanPlacarComputador.innerText =  pontosDoComputador;
+    spanPlacarUsuario.innerText = pontosDoUsuario;
+}
+
+atualizarPlacar(3,2)
+
